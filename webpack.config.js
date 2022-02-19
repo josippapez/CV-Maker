52911c4e3790d15718a88338f1baf292f8388620
Josip <papezjosip@gmail.com>
2022-02-19 14:21:52 +0100

    added github action for build, created build

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+  mode: devMode ? "development" : "production",
   entry: './src/index.tsx',
   target: 'web',
   output: {
