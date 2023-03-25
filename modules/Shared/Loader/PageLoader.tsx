909423e22ca308d74a8e8f9c06d7137f5b45109a
Josip Papež <papezjosip@gmail.com>
2023-03-25 10:15:31 +0100

    Changes to page loader that should fix SEO

--- modules/Shared/Loader/PageLoader.tsx
+++ modules/Shared/Loader/PageLoader.tsx
@@ -6,15 +6,16 @@ interface Props {
 }
 
 export const PageLoader = ({ children, isLoading, inline }: Props) => {
-  return isLoading ? (
-    <div
-      className={`${
-        !inline ? 'fixed' : ''
-      } top-0 right-0 left-0 bottom-0 z-50 flex h-full w-full items-center justify-center bg-white dark:bg-neutral-700`}
-    >
-      <div className={`${style.loader}`} />
-    </div>
-  ) : (
-    <>{children}</>
+  return (
+    <>
+      <div
+        className={`${!inline ? 'fixed' : ''} ${
+          isLoading ? '' : 'hidden'
+        } top-0 hi right-0 left-0 bottom-0 z-50 flex h-full w-full items-center justify-center bg-white dark:bg-neutral-700`}
+      >
+        <div className={`${style.loader}`} />
+      </div>
+      {children}
+    </>
   );
 };
