f3ec1e43e4302b7146488d811ba373e677132cd7
Josip Papež <papezjosip@gmail.com>
2023-05-06 22:25:08 +0200

    Updated template 4 line height

--- modules/PDFView/CVTemplates/Templates/Template4.tsx
+++ modules/PDFView/CVTemplates/Templates/Template4.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     fontWeight: 'light',
     fontSize: 10,
     color: 'white',
-    lineHeight: 2
+    lineHeight: 1.5
   },
   column: {
     flexDirection: 'column',
